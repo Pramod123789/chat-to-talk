d46cdfd1dd5de4d113ee384798f9a7e6c4e5907e
pramod123789 <pramodgupta12378@gmail.com>
2023-10-19 16:42:28 +0530

    added fronted

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
 const express = require("express");
-
+const { chats } = require("./data/data");
 const app = express()
 
+app.get('/', (req,res) => {
+
+    res.send("API is Running");
+
+});
+
+app.get('/api/chat',(req,res) =>
+{
+    res.send(chats);
+});
+
 app.listen(5000, console.log("sever started on port 5000"));
+
 
 
